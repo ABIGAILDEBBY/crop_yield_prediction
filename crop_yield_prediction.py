23fcac56e190a7ffb1b707c0a404adb38f0607ca
Abigail Woolley <woolleyabigail47@gmail.com>
2024-07-04 00:45:47 +0000

    Reformatted file

--- crop_yield_prediction.py
+++ crop_yield_prediction.py
@@ -117,7 +117,9 @@ def split_data(X, y, test_size=0.2, random_state=42):
     """
 
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
 
     return X_train, X_test, y_train, y_test
 
